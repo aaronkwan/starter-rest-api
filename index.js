223732974c67ca7009d109f534222053220c8a76
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2024-04-18 16:04:59 -0700

    add CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
